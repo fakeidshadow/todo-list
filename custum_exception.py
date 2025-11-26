eeaeffc5bd06109ccecb355e3a462effac7aa647
fakeidshadow <mmghasemi13@gmail.com>
2025-11-26 16:27:06 +0330

    init: initail alembic and database

--- custum_exception.py
+++ custum_exception.py
@@ -1,4 +1,4 @@
 class CustomError(Exception):
-    def __init__(self, message, error_code):
+    def __init__(self, message, error_code=1):
         super().__init__(message)
         self.error_code = error_code
---
+++ models/user.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+metadata = Base.metadata
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
