b304bca2a7996976b55e9ae68e3cea7cedd236e8
fakeidshadow <mmghasemi13@gmail.com>
2025-12-05 23:39:08 +0330

    feat: adding add functions

--- models.py
+++ models.py
@@ -18,7 +18,7 @@ class Project(Base):
     __tablename__ = 'project'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(30))
+    name = Column(String(30), unique=True)
     description = Column(String(150))
 
     tasks = relationship("Task", back_populates="project")
@@ -31,7 +31,7 @@ class Task(Base):
 
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(30))
+    name = Column(String(30), unique=True)
     status = Column(Integer, default=0)
     deadline = Column(String(20))
     description = Column(String(150))
@@ -40,3 +40,43 @@ class Task(Base):
     owner = Column(Integer, ForeignKey('project.id'))
 
     project = relationship("Project", back_populates="tasks")
+
+
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+engine = create_engine('postgresql+psycopg2://postgres:sec123@localhost:5432/todo_db', echo=True)
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+def add_user(name:str) -> int:
+    user = User(name=name)
+    session.add(user)
+    session.commit()
+
+def add_project(username:str, name:str, description:str) -> int:
+    user = get_user(name)
+    project = Project(name=name, description=description, owner=user.id)
+    session.add(project)
+    session.commit()
+
+def add_task(project_name:str, name:str, status:int, deadline:str, description:str, closed_at:str):
+    project = get_project(name=project_name)
+    task = Task(name=name, status=status, deadline=deadline, description=description, closed_at=closed_at, owner=project.id)
+    session.add(task)
+    session.commit()
+
+def get_user(name:str) -> User:
+    pass
+
+def get_project(name:str) -> Project:
+    pass
+
+def get_task(name:str) -> Task:
+    pass
